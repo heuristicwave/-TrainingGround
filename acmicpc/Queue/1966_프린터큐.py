a99fbe2db2872fca831b4498d355fc9dca8ddcd6
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2020-03-29 18:28:16 +0900

    Using max() to create an effect like a priority queue

--- acmicpc/Queue/1966_프린터큐.py
+++ acmicpc/Queue/1966_프린터큐.py
@@ -6,6 +6,11 @@ for _ in range(test_case):
     # (i, idx) : index is tuple second element
     queue = [(i, idx) for idx, i in enumerate(queue)]
 
+    # print(queue)
+    # tc_1 : [(5, 0)]
+    # tc_2 : [(1, 0), (2, 1), (3, 2), (4, 3)]
+    # tc_3 : [(1, 0), (1, 1), (9, 2), (1, 3), (1, 4), (1, 5)]
+
     count = 0
     while True:
         # queue front == the most priority document
@@ -17,7 +22,9 @@ for _ in range(test_case):
                 print(count)
                 break
             else:
-                queue.pop(0)  # line 20과 23의 차이 정확히!
+                # print(queue) # [(4, 3), (1, 0), (2, 1), (3, 2)]
+                queue.pop(0)  # del queue[0]
+                # print(queue) # [(1, 0), (2, 1), (3, 2)]
 
         else:
             queue.append(queue.pop(0))
