14536a3ffab67ad469bd5e2c460cca5572c3ed54
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2020-06-06 21:15:00 +0900

    Create 2437_저울.py

---
+++ acmicpc/Greedy/2437_저울.py
@@ -0,0 +1,12 @@
+N, A = int(input()), sorted(list(map(int, input().split())))
+
+ans = 0
+
+for i in A:
+    # Key Point!
+    if i <= ans + 1:
+        ans += i
+    else:
+        break
+
+print(ans+1)
