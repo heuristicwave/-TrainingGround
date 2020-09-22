d89025b61a323c0c98b1f55da40f962afe2e1945
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2020-09-23 00:08:43 +0900

    우선순위큐 마이너스

---
+++ acmicpc/Queue/11279_최대힙.py
@@ -0,0 +1,15 @@
+import heapq
+
+N = int(input())
+A = [int(input()) for _ in range(N)]
+
+hq = []
+
+for i in range(N):
+    if not A[i]:  # 0 일때
+        if not len(hq):
+            print(0)
+        else:
+            print(heapq.heappop(hq)[1])
+    else:
+        heapq.heappush(hq, [-A[i], A[i]])
