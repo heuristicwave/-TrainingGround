857d8c8d7ae3831b34fcc4be9d9728ed1f18ed4b
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2020-11-07 12:40:34 +0900

    visited 체크 위치에 따라 중복 체크 여부

---
+++ acmicpc/BackTracking/14888_연산자끼워넣기.py
@@ -0,0 +1,42 @@
+
+# 코드 출처 : https://velog.io/@seovalue
+
+from itertools import permutations
+
+
+def calc(numbers, operator):
+    answer = numbers[0]
+    for n, op in zip(numbers[1:], operator):
+        if op == '+':
+            answer += n
+        elif op == '*':
+            answer *= n
+        elif op == '-':
+            answer -= n
+        elif op == '/':
+            if answer < 0:  # 음수를 양수로 나눌 때
+                answer = -1 * (abs(answer) // n)
+            else:
+                answer //= n
+    return answer
+
+
+n = int(input())
+numbers = list(map(int, input().split()))
+num_of_op = list(map(int, input().split()))
+sample_op = ['+', '-', '*', '/']
+operator = []
+
+for i, j in zip(sample_op, num_of_op):
+    print(i, j)
+    if j >= 1:  # 연산자의 개수가 1이상이면, 개수만큼 operator에 삽입
+        for _ in range(j):
+            operator.append(i)
+
+permutation_list = list(set((permutations(operator, len(operator)))))
+answer = []
+for p in permutation_list:
+    answer.append(calc(numbers, p))
+
+print(max(answer))
+print(min(answer))
